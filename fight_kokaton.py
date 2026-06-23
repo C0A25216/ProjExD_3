d69844b1a2fb9f87b1b0f992009ff8904a961f01
haruto <c0a2521607@edu.teu.ac.jp>
2026-06-23 14:37:31 +0900

    練習3：喜びエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,6 +162,8 @@ def main():
         if bomb is not None:
             if beam is not None:
                 if beam.rct.colliderect(bomb.rct):
+                    bird.change_img(6, screen)
+                    pg.display.update()
                     beam = None
                     bomb = None
         
